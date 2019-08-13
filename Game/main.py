4efbfe23dd0a828264becf4aeac69fcfcfc3bd2f
PhiCoding <development@phicoding,com.br>
2019-08-13 15:25:59 -0300

    Init pygame

--- Game/main.py
+++ Game/main.py
@@ -1,14 +1,19 @@
 #sprint open source 0x72.itch.io/dungeontileset-ii (local onde peguei os personagens)
 
 import sys, pygame
+pygame.init()
 
-tamanho = width, height = 800, 600
-tela = pygame.display.set_mode(tamanho)
 personagem_nome = 'balu'
 personagem_coracao_maximo = 10
 personagem_coracao_inicial = 3
 personagem_coracao_momento = personagem_coracao_inicial
+
+
+tamanho = width, height = 800, 600
 cor = 255, 255, 0
+tela = pygame.display.set_mode(tamanho)
+
+
 
 while 1:
     for event in pygame.event.get():

2fa5f0825b987282a33b9925cd447a1fb5919584
carolbarbosa <54076902+carolbarbosa@users.noreply.github.com>
2019-08-13 15:58:37 -0300

    personagem na tela

--- Game/main.py
+++ Game/main.py
@@ -7,7 +7,8 @@ personagem_nome = 'balu'
 personagem_coracao_maximo = 10
 personagem_coracao_inicial = 3
 personagem_coracao_momento = personagem_coracao_inicial
-
+personagem_avatar = pygame.image.load('personagens/heroi/h1.png')
+personagem_posicao = personagem_avatar.get_rect()
 
 tamanho = width, height = 800, 600
 cor = 255, 255, 0
@@ -17,8 +18,10 @@ tela = pygame.display.set_mode(tamanho)
 
 while 1:
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.QUIT: sys.exit()
     tela.fill(cor)
-
-
+    tela.blit(personagem_avatar, personagem_posicao)
+    pygame.display.flip()
+    
 

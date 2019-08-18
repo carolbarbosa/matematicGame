81fc1f3cbede05996d09ab96aeaa8f14820eef87
carolbarbosa <54076902+carolbarbosa@users.noreply.github.com>
2019-08-18 01:01:53 -0300

    Inserindo corações

--- Game/main.py
+++ Game/main.py
@@ -11,6 +11,12 @@ personagem_coracao_momento = personagem_coracao_inicial
 imagemFundo = pygame.image.load('personagens/heroi/imagemFundo.png')
 personagem_avatar = pygame.image.load('personagens/heroi/h1.png')
 personagem_posicao = personagem_avatar.get_rect()
+coracao = pygame.image.load('personagens/heroi/coraçao.png')
+coracao = pygame.image.load('personagens/heroi/coraçao.png')
+coracao = pygame.image.load('personagens/heroi/coraçao.png')
+coracao = pygame.image.load('personagens/heroi/coraçao.png')
+
+
 
 tamanho = width, height = 800, 600
 cor = 255, 255, 0
@@ -46,6 +52,12 @@ while 1:
     tela.fill(cor)
     tela.blit(imagemFundo,(0,0))
     tela.blit(personagem_avatar, personagem_posicao)
+    tela.blit(coracao,(0,0))
+    tela.blit(coracao,(50,0))
+    tela.blit(coracao,(100,0))
+    tela.blit(coracao,(150,0))
+    tela.blit(coracao,(200,0))
+
     relogio.tick(27)
     pygame.display.flip()
 
